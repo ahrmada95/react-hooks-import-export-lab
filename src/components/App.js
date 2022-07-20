ebdbf5c8ee36bd49e9706a3713b53a5ab55312ac
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-07-20 09:48:32 -0400

    commit tree 860812c41368ae3fc0e57852937920b770dad16e

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import NavBar from "./NavBar"
+import Home from "./Home"
+import About from "./About"
+import { username, city } from "../data/user"
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Home usr={ username } cty={ city }/> {/* this is how you pass parameters to a component*/}
       <About />
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,3 +7,7 @@ function NavBar() {
     </nav>
   );
 }
+
+export default NavBar
+// export default NavBar //this will get overrided if you have ANY other export!
+// export { x } //another way to do export, you can do it one at a time or compounded
